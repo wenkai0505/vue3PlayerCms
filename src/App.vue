dfd7f88b4fe142fecdbbb0a33798f5ff4ab0e333
Ryan <>
2026-07-03 10:27:11 +0800

    feat(setup): 整合 Element Plus、Vue Router 與 SCSS 基礎架構

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "@/styles/main.scss";
+import { RouterView } from "vue-router";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Layout">
+        <div class="box"></div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.box {
+    @include box(300px, 300px);
+    @include flex;
+    background: var(--color-brand);
+    color: var(--color-text);
+}
+</style>
