153f9f5edb8fd2924221fb22b8774c18087502c6
Ryan <>
2026-07-08 14:06:14 +0800

    修改發布設定

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import router from "@/router";
 
 async function bootstrap() {
-    if (import.meta.env.DEV && import.meta.env.VITE_USE_MSW === "true") {
+    if (import.meta.env.VITE_USE_MSW === "true") {
         const { worker } = await import("@/mocks/browser");
         await worker.start({
             onUnhandledRequest: "bypass",
